dce218f215c479d5b156ad84f63fceb4bd69516a
Antonio Manzari <antoniomanzari@gmail.com>
2022-06-25 20:30:28 -0400

    finished up to 18:28, made dynamic viewport boundaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import * as THREE from 'three'
 import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 
-function Box() {
+function Box({z}) {
   const ref = useRef()
-  const [clicked, setClicked] = useState(false)
+  const { viewport, camera } = useThree()
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
+
+
+  const [data] = useState(
+    {
+      x: THREE.MathUtils.randFloatSpread(2),
+      y: THREE.MathUtils.randFloatSpread(height),
+    }
+  )
 
   /* update loop */
+
   useFrame((state)=> {
-    ref.current.position.z = THREE.MathUtils.lerp(ref.current.position.z, clicked ? 1 : 0, 0.1)
+    ref.current.position.set(data.x * width, (data.y += 0.01), z)
+    if (data.y > height * 0.7) {
+      data.y = -height * 0.7
+    }
   })
 
   return (
-    <mesh ref={ref} onClick={() => setClicked(!clicked) }>
+    <mesh ref={ref}>
       <boxGeometry/>
       <meshBasicMaterial color='orange' />
     </mesh>
   )
 }
 
-export default function App() {
+export default function App({ count = 100 }) {
   return (
     <Canvas>
-      <Box />
+      { Array.from({ length: count }, (_, i) => (<Box key={i} z={-i} />))}
     </Canvas>
   )
 }
